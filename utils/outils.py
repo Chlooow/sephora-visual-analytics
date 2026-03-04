2987251644b6e48f0ea9e5d3d04c5b44a8ea2e4e
Eclow <chloe.makoundou@universite-paris-saclay.fr>
2026-03-04 17:32:39 +0100

    Nettoyage de mon notebook pour le rendu
    J'ai nettoyé mon nnotebook pour mettre les fonctions utils dans un fichier a part afin que le code sois plus modulaire. Maintenant il faut passer sur Tableau

--- utils/outils.py
+++ utils/outils.py
@@ -26,3 +26,72 @@ def extraction_ml(size_str):
     if oz_match:
         return round(float(oz_match.group(1)) * 29.5735, 2)
     return None 
+
+# nettoyage de "how to use" pour en faire une colonne binaire : instruction ou pas d'instruction
+def clean_how_to_use(text):
+    if pd.isna(text):
+        return False
+    
+    s = str(text).lower().strip()
+
+    # si ca commence par suggested usage = TRUE
+    if 'suggested usage' in s:
+        return True
+    
+    # sinon c'est no instruction = False
+    if 'no instruction' in s or s == '' or s == 'unknown':
+        return False
+    
+    # securité de logique 
+    return len(s) > 20
+
+
+# normalisation de la colonne d'options
+def count_options(options_str):
+    if pd.isna(options_str) or str(options_str).lower().strip() == 'no options':
+        return 1
+    
+    s = str(options_str).strip()
+
+    # on compte par nombre de -
+    parts = [p for p in s.split(' - ') if p.strip()]
+
+    return max(len(parts), 1)
+
+# ajout d'une colonne gamme en fonction du value_price
+def segment_prix(price):
+    if price < 20:
+        return 'access'
+    elif price <= 50:
+        return 'middle'
+    elif price <= 100:
+        return 'premium'
+    else:
+        return 'luxury'
+
+# ajout du "brand_age"
+def calculate_brand_age(year):
+    try:
+        y = int(year)   
+        # precond : year aberrante
+        if y <= 0 or y > 2026:
+            return None
+        return 2026 - y
+    except (ValueError, TypeError):
+        return None
+
+# determiner le statut de la marque grace a son age
+def brand_status(brandage):
+    if brandage is None :
+        return None
+    elif brandage <= 5:
+        return 'rookie'
+    elif brandage <= 10:
+        return 'emerging'
+    elif brandage <= 30:
+        return 'established'
+    elif brandage <= 70:
+        return 'classic'
+    else :
+        return 'heritage'
+    
